859f812de15c330e5b4b42fdf7b527dded66e10f
Galym Tlektesov <galym2000gt@gmail.com>
2020-04-17 15:56:53 +0600

    Сделан 6-ой уровень, и главное меню

---
+++ Assets/Script/ClickText.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ClickText : MonoBehaviour
+{
+    private RectTransform rect;
+    private Text text;
+    public float speed;
+    public PlayerController player;
+    private bool isEnd = false;
+
+    private void Start()
+    {
+        rect = GetComponent<RectTransform>();
+        text = GetComponent<Text>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) && isEnd)
+        {
+            SceneManager.LoadScene(0, LoadSceneMode.Single);
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        isEnd = true;
+        player.enabled = false;
+        rect.anchorMax = new Vector2(1, 1);
+        text.fontSize = 150;
+    }
+}
---
+++ Assets/Script/KeyText.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class KeyText : MonoBehaviour
+{
+    public Text text;
+
+    private void Start()
+    {
+        text.gameObject.active = false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        text.gameObject.active = true;
+    }
+}
