26f3f855831f48919d1f013df64f314b3fb08f9e
Galym Tlektesov <galym2000gt@gmail.com>
2020-04-30 18:53:00 +0600

    Исправил ошибки

--- Assets/Script/DragPlayer.cs
+++ Assets/Script/DragPlayer.cs
@@ -2,10 +2,30 @@
 
 public class DragPlayer : MonoBehaviour
 {
+    public Rigidbody2D rb;
+    public bool isDrag = false;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
 
     private void OnMouseDrag()
     {
-        var mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        transform.position = new Vector2(mousePos.x, mousePos.y);
+        isDrag = true;
+    }
+
+    private void OnMouseUp()
+    {
+        isDrag = false;
+    }
+
+    private void FixedUpdate()
+    {
+        if (isDrag)
+        {
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            rb.MovePosition(mousePos + rb.position * Time.fixedDeltaTime);
+        }
     }
 }
--- Assets/Script/MouseController.cs
+++ Assets/Script/MouseController.cs
@@ -2,14 +2,14 @@
 
 public class MouseController : MonoBehaviour
 {
+    public Transform player;
     public int sceneName = 5;
     public float speed = 2.5f;
-    private static bool key;
 
 
-    private void Update()
+    private void OnMouseDown()
     {
         var mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        transform.position = Vector2.MoveTowards(transform.position, new Vector2(mousePos.x, mousePos.y), speed * Time.deltaTime);
+        player.position = Vector2.Lerp(player.position, new Vector2(mousePos.x, mousePos.y), speed * Time.deltaTime);
     }
 }
